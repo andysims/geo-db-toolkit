f8bedefd03e6e2d22233d8c6e11476b08ddedb90
Andy <geogeek93@gmail.com>
2026-05-29 16:02:14 -0700

    Added new scripts

--- ArcGISManagement/Content/Content.psm1
+++ ArcGISManagement/Content/Content.psm1
@@ -21,5 +21,42 @@ function Get-ArcGISContentSummary {
     & (Resolve-ContentScriptPath "Get-ContentSummary.ps1") @PSBoundParameters
 }
 
+# ---------------------------------------------------------
+# Get-NewArcGISItems
+# ---------------------------------------------------------
+function Get-NewArcGISItems {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [ValidateSet("portal", "agol")]
+        [string]$Source,
 
-Export-ModuleMember -Function Get-ArcGISContentSummary
+        [int]$Days = 7
+    )
+
+    & (Resolve-ContentScriptPath "Get-NewArcGISItems.ps1") @PSBoundParameters
+}
+
+# ---------------------------------------------------------
+# Find-ArcGISContent
+# ---------------------------------------------------------
+function Find-ArcGISContent {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [ValidateSet("portal", "agol")]
+        [string]$Source,
+
+        [string]$Owner,
+        [string]$Title,
+        [string]$Id,
+        [string]$Type
+    )
+
+    & (Resolve-ContentScriptPath "Find-ArcGISContent.ps1") @PSBoundParameters
+}
+
+Export-ModuleMember -Function `
+    Get-ArcGISContentSummary, `
+    Get-NewArcGISItems, `
+    Find-ArcGISContent
