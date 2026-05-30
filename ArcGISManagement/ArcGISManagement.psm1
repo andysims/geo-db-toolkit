e1e994e09de870e2bb0220dd1b19e29385d74a27
Andy <geogeek93@gmail.com>
2026-05-29 17:36:10 -0700

    Create ArcGISManagement.psm1

---
+++ ArcGISManagement/ArcGISManagement.psm1
@@ -0,0 +1,6 @@
+# ArcGISManagement.psm1
+$root = $PSScriptRoot
+
+Import-Module "$root\Users\Users.psm1" -Force
+Import-Module "$root\Content\Content.psm1" -Force
+Import-Module "$root\Groups\Groups.psm1" -Force
